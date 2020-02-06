a6281b54647ed3bb1badcc694f25c8368a726dfc
Shivam Pundir <shivampundir@rocketmail.com>
2020-02-06 14:52:19 +0530

    Added Lambda Expressions instead of anonymous classes

--- src/main/java/org/example/sweet/Sweet.java
+++ src/main/java/org/example/sweet/Sweet.java
@@ -30,34 +30,23 @@ public abstract class Sweet implements Sort {
     public void sort(List<Sweet> sweets, String param) {
         switch (param) {
             case "name":
-                sweets.sort(new Comparator<Sweet>() {
-                    @Override
-                    public int compare(Sweet o1, Sweet o2) {
-                        return o1.getName().compareTo(o2.getName());
-                    }
-                });
+                sweets.sort(Comparator.comparing(Sweet::getName));
                 break;
             case "price":
-                sweets.sort(new Comparator<Sweet>() {
-                    @Override
-                    public int compare(Sweet e1, Sweet e2) {
-                        if (e1.price == e2.price) {
-                            return e1.name.compareTo(e2.name);
-                        } else {
-                            return e1.price > e2.price ? 1 : -1;
-                        }
+                sweets.sort((e1, e2) -> {
+                    if (e1.price == e2.price) {
+                        return e1.name.compareTo(e2.name);
+                    } else {
+                        return e1.price > e2.price ? 1 : -1;
                     }
                 });
                 break;
             case "weight":
-                sweets.sort(new Comparator<Sweet>() {
-                    @Override
-                    public int compare(Sweet e1, Sweet e2) {
-                        if (e1.weight == e2.weight) {
-                            return e1.name.compareTo(e2.name);
-                        } else {
-                            return e1.weight > e2.weight ? 1 : -1;
-                        }
+                sweets.sort((e1, e2) -> {
+                    if (e1.weight == e2.weight) {
+                        return e1.name.compareTo(e2.name);
+                    } else {
+                        return e1.weight > e2.weight ? 1 : -1;
                     }
                 });
                 break;
